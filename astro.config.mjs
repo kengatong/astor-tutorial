d1ffadb5c86e1104d1c8a1f6310ae0c4580d03fd
kenga <kengatong@qq.com>
2025-10-03 21:09:22 +0800

    添加UI框架Preact，使其可以交互

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://kenga-as.netlify.app"
+  site: "https://kenga-as.netlify.app",
+  integrations: [preact()]
 });
